09b06b45eb89f855d71f1927bee6013b28103b4a
Nishikaru <mdsajed001@gmail.com>
2023-03-27 10:20:40 +0600

    new mobile view look

--- src/components/posts/posts.tsx
+++ src/components/posts/posts.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const Posts: React.FC<Props> = ({item}) => {
   return (
-    <div className='w-full bg-white_gray-100 dark:bg-black_gray-500 shadow-md py-2 px-4 flex   items-center gap-5'>
+    <div className='w-full bg-white_gray-100 dark:bg-black_gray-500 shadow-md py-2 px-4 flex flex-col lg:flex-row  items-center gap-5'>
       <div className='relative'>
       <img className='' src={item.img} alt="" />
       {/* <div className='absolute w-full h-[60%] text-lg text-white_gray-100 flex flex-col-reverse bottom-0 bg-gradient-to-t from-[#1a1a1b] to-[#1a1a1b00]'></div> */}
